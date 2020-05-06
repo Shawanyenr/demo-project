a855cf93c16e161b1b7ca19879dcc43ccfb17633
Obi-Wan <shawanyenr@gmail.com>
2020-05-06 11:55:19 +0800

    查询与某人聊过天的用户且有最后一条消息，并以消息时间倒序排序

--- springboot_shixun.sql
+++ springboot_shixun.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 05/05/2020 10:19:24
+ Date: 06/05/2020 11:55:09
 */
 
 SET NAMES utf8mb4;
@@ -47,7 +47,7 @@ CREATE TABLE `blocklist`  (
   `bid` int(20) NOT NULL,
   `time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of blocklist
